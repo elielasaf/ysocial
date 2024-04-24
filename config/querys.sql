eb5a05797dfc75eefc2845eda723cd54f717bbd3
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-24 19:34:38 -0400

    Darlin: Creando apartado de creación de publicaciones (1/?)

--- config/querys.sql
+++ config/querys.sql
@@ -6,3 +6,12 @@ CREATE TABLE users (
     user_correo VARCHAR(256) NOT NULL,
     user_contraseña VARCHAR(256) NOT NULL
 );
+
+/*Aun faltan columnas*/
+CREATE TABLE posts (
+    post_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    post_title VARCHAR(124) NOT NULL,
+    post_desc VARCHAR(264) NOT NULL,
+    post_img LONGTEXT NOT NULL,
+    post_by INT NOT NULL
+);
