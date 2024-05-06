bd6594d90e76e50b2c1901ae4cfe271ed8b15d43
elielasaf <diazamadorelielasaf@gmail.com>
2024-05-06 09:21:39 -0400

    Error.

--- config/db.php
+++ config/db.php
@@ -1,4 +1,4 @@
 <?php
     //$db = new PDO("mysql:host=localhost; dbname=ysocial;", "root", "");
-    $db = new PDO("mysql:host=localhost; dbname=u849861573_ysocial;", "u849861573_yadmin", "Ysocial_Admin_123");
+    $db = new PDO("mysql:host=localhost; dbname=u849861573_ysocial;", "u849861573_yadmin", "Ysocial_Admin123");
 ?>
