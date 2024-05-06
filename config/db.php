257fc51c00c502d0a057a57d027f20d4b24760d5
elielasaf <diazamadorelielasaf@gmail.com>
2024-05-06 09:19:40 -0400

    Enlazando la db.

--- config/db.php
+++ config/db.php
@@ -1,4 +1,4 @@
 <?php
-    $db = new PDO("mysql:host=localhost; dbname=ysocial;", "root", "");
-    //$db = new PDO("mysql:host=localhost; dbname=ysocial;", "root", "Darlin_123");
+    //$db = new PDO("mysql:host=localhost; dbname=ysocial;", "root", "");
+    $db = new PDO("mysql:host=localhost; dbname=u849861573_ysocial;", "u849861573_yadmin", "Ysocial_Admin_123");
 ?>
